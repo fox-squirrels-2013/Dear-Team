744e5c79293a7915d8478b4d041d5a37b91338b6
Apprentice <apprentice@dbc10.local>
2013-11-05 17:06:29 -0800

    Wrote out basic logic for MVP

---
+++ team.js
@@ -0,0 +1,15 @@
+var test_phrase = "Hello World"
+
+console.log(test_phrase)
+
+// var beginning = Time()
+
+var input = prompt("Type that as fast as you can")
+// Here the coder assumes that the code after prompt will not execute until the user has hit enter on the prompt.
+
+// var end = Time()
+
+// compare test_phrase to input and return accuracy
+//return accuracy
+console.log("Your score was " + accuracy)
+
