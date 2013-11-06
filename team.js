38db04c4d6d9e81a82a097b652e45cc3a49e53f7
raorao <savinirs@gmail.com>
2013-11-06 15:57:07 -0800

    Rough start

--- team.js
+++ team.js
@@ -1,18 +1,22 @@
 var test_phrase = "Hello World"
 
-document.write(test_phrase)
+document.write("Please type the test phrase: " + test_phrase)
 // document.write(<br>)
 var start = Date.now();
 // var input = prompt("Type that as fast as you can")
 var response = prompt("Type the phrase " + "'" + test_phrase + "'");
 var end = Date.now();
 
-if (test_phrase){
-
+if (test_phrase === response){
+  var answer = "you got it right!";
+}
+else{
+  var answer = "you got it wrong!";
 }
 
 var elapsed = (end - start) / 1000;
-document.write(" You took " + elapsed + " seconds" + " to type: " + response);
+
+document.write(" You took " + elapsed + " seconds" + " to type: " + response + " and " + answer);
 
 // // Here the coder assumes that the code after prompt will not execute until the user has hit enter on the prompt.
 
