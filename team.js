b7a75b4e4763a61fc91b04473c338bf950258062
Apprentice <apprentice@dbc15.local>
2013-11-07 23:33:16 -0800

    Dom manipulation, HTML, changed accuracy function

--- team.js
+++ team.js
@@ -1,37 +1,39 @@
-var test_phrase = "Hello World"
+window.onload=function(){
 
-document.write("Please type the test phrase: " + test_phrase)
+//var test_phrase = "Hello World"
+
+//document.write("Please type the test phrase: " + test_phrase)
 // document.write(<br>)
 var start = Date.now();
 // var input = prompt("Type that as fast as you can")
-var response = prompt("Type the phrase " + "'" + test_phrase + "'");
+// var response = prompt("Type the phrase " + "'" + test_phrase + "'");
 var end = Date.now();
 
-if (test_phrase === response){
-  var answer = "you got it right!";
-}
-else{
-  var answer = "you got it wrong!";
-}
+// if (test_phrase === response){
+//   var answer = "you got it right!";
+// }
+// else{
+//   var answer = "you got it wrong!";
+// }
 
 var elapsed = (end - start) / 1000;
 
-document.write(" You took " + elapsed + " seconds" + " to type: " + response + " and " + answer);
+//document.write(" You took " + elapsed + " seconds" + " to type: " + response + " and " + answer);
 
-// // Here the coder assumes that the code after prompt will not execute until the user has hit enter on the prompt.
+// Here the coder assumes that the code after prompt will not execute until the user has hit enter on the prompt.
 
-// // compare test_phrase to input and return accuracy
-// //return accuracy
+// compare test_phrase to input and return accuracy
+//return accuracy
 // console.log("Your score was " + accuracy)
 
 //_________________________miya____________________________
+
 // ACCURACY FUNCTION
-// assigning correct and incorrect variable to start at 0
+
 var correct = 0;
 var incorrect = 0;
 
-// assuming Lindsey's code will return a "result" true or false at each compared letter
-//var result = true;
+var result = true
 
 function accuracy(result) {
   if (result) {
@@ -42,4 +44,26 @@ function accuracy(result) {
     incorrect += 1;
     return incorrect
   }
+}
+
+accuracy(result)
+
+var accuracy_counter = document.getElementById('accuracy_score');
+accuracy_counter.innerHTML = 'this will be the accuracy score'
+
+var time_counter = document.getElementById('time');
+time_counter.innerHTML = elapsed
+
+var correct_counter = document.getElementById('correct_score');
+correct_counter.innerHTML = correct
+
+var incorrect_counter = document.getElementById('incorrect_score');
+incorrect_counter.innerHTML = incorrect
+
+var string_display = document.getElementById('test_string');
+string_display.innerHTML = "this will connect to the 'getString' function / HTML 'test_string'"
+
+var game_finished_display = document.getElementById('game_finished');
+game_finished_display.innerHTML = "this will connect to the 'gameFinished?'' function / HTML"
+
 }
