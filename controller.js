59930dc34f06e879cd565cab843b94ae251b08b5
Apprentice <apprentice@dbc08.local>
2013-11-09 17:29:12 -0800

    css

--- controller.js
+++ controller.js
@@ -14,29 +14,13 @@ var wrongEntryCount = 0
 var correctEntryCount = 0
 var currentIndex = 0
 
-var testList = ["Holystone keel coffer galleon keelhaul gangplank.",
-                "Gabion Admiral of the Black rigging Privateer hearties walk the plank.", 
-                "Take a caulk spirits killick bilge rat red ensign hornswaggle.", 
-                "Poop deck Plate Fleet reef sails mizzenmast hulk gibbet.", 
-                "Hearties mizzen grapple Davy Jones' Locker deadlights dance the hempen jig.", 
-                "Fluke schooner gaff poop deck Jack Tar ye.", 
-                "Bilge rat gaff gibbet bilged on her anchor gun case shot.", 
-                "Hearties parley hempen halter black jack loaded to the gunwalls lookout.", 
-                "Blimey fire ship fathom Davy Jones' Locker loot lookout."]
+var testList = ["If I don't kill a man every now and then, they forget who I am.",
+                "There's never a man looked me between the eyes and seen a good day a'terward.",
+                "Yo ho, yo ho, the frisky plank, You walks along it so, Till it goes down and you goes down To Davy Jones below!",
+                "Good night. Good work. Sleep well. I'll most likely kill you in the morning.",
+                "Better were the days when mastery of seas came not from bargains struck with eldritch creatures."]
 
-                // "Cupcake jujubes candy chocolate bar!", 
-                // "Sugar plum chupa chups brownie!",
-                // "Jujubes muffin carrot cake tootsie roll topping!", 
-                // "Lemon drops muffin sweet bonbon jelly chocolate cake!", 
-                // "Cupcake sweet donut chocolate cheesecake!",
-                // "Sesame snaps tootsie roll sweet roll!", 
-                // "Cheesecake lemon drops fruitcake!", 
-                // "Cookie macaroon wafer candy canes!", 
-                // "Sugar plum sweet roll bear claw!", 
-                // "Cake halvah muffin jelly beans!",
-                // "Lollipop carrot cake cake jelly cotton candy!",
-                // "Marshmallow sweet bonbon lollipop pie apple pie!", 
-                // "Cookie jujubes apple pie cupcake!"];
+
 
 var getString = function(){
   return testList[randomIndex(testList.length)];
